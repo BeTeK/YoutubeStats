4afd9b611411f50d064b66d93841363944c60901
Pasi Luostarinen <betek1@gmail.com>
2017-04-26 11:50:12 +0300

    initial commit

---
+++ dev/Youtube.py
@@ -0,0 +1,85 @@
+from apiclient.discovery import build
+
+class Youtube:
+    def __init__(self, key):
+        self.youtube = build("youtube", "v3", developerKey=key)
+        self.gategoryIdCache = {}
+
+    def getChannelPlaylists(self, channelName):
+        params = {"part": "id", "maxResults": 50, "channelId": channelName}
+        results = self._iterateAllItems(self.youtube.playlists().list, params)
+
+        return [i["id"] for i in results]
+
+    def getItemsInPlaylist(self, playlistId):
+        params = {"part": "snippet", "maxResults": 50, "playlistId": playlistId}
+        results = self._iterateAllItems(self.youtube.playlistItems().list, params)
+
+        return [i["snippet"]["resourceId"]["videoId"] for i in results]
+
+    def getChannelInfo(self, channelIds):
+        results = {}
+        for chunk in self._chunks(channelIds, 50):
+            for key, value in self._getChannelInfoImpl(chunk).items():
+                results[key] = value
+
+        return results
+
+    def _getChannelInfoImpl(self, channelIds):
+        params = {"part": "statistics,snippet", "maxResults": 50, "id": ",".join(channelIds)}
+        results = self._iterateAllItems(self.youtube.channels().list, params)
+        
+        return {i["id"] : {"statistics": i["statistics"], "snippet": i["snippet"]} for i in results}
+        
+    
+    def getVideoStatistics(self, videoIds):
+        results = {}
+        for chunk in self._chunks(videoIds, 50):
+            for key, value in self._getVideoStatisticsImpl(chunk).items():
+                results[key] = value
+
+        return results
+    
+    def _getVideoStatisticsImpl(self, videoIds):
+        params = {"part": "statistics,snippet", "maxResults": 50, "id": ",".join(videoIds)}
+        results = self._iterateAllItems(self.youtube.videos().list, params)
+        
+        return {i["id"] : {j[0] : j[1] for j in list(i["statistics"].items()) + list(i["snippet"].items())} for i in results}
+
+
+    def getVideoGategory(self, gategoryId):
+        if gategoryId not in self.gategoryIdCache:
+            result = self.youtube.videoCategories().list(part="snippet", hl="en_US", id=gategoryId).execute()
+            self.gategoryIdCache[gategoryId] = result["items"][0]["snippet"]["title"]
+
+        return self.gategoryIdCache[gategoryId]
+
+    def getChannelId(self, nameOrId):
+        user = self.youtube.channels().list(part="id", forUsername=nameOrId).execute()
+        chId = self.youtube.channels().list(part="id", id=nameOrId).execute()
+
+        if len(user["items"]) > 0:
+            return user["items"][0]["id"]
+        if len(chId["items"]) > 0:
+            return chId["items"][0]["id"]
+        
+        return None
+
+    def _iterateAllItems(self, fn, params):
+        results = fn(**params).execute()
+        
+        out = results["items"]
+        
+        if "nextPageToken" in results:
+            nextParams = dict(params)
+            nextParams["pageToken"] = results["nextPageToken"]
+            
+            out += self._iterateAllItems(fn, nextParams)
+
+        return out
+
+
+    def _chunks(self, l, n):
+        n = max(1, n)
+        return (l[i:i+n] for i in range(0, len(l), n))
+    
---
+++ dev/main.py
@@ -0,0 +1,71 @@
+import apiKey
+import Youtube
+import googleapiclient
+import googleapiclient.errors
+import sys
+
+def countGategories(stats):
+    results = {}
+    for key, value in stats.items():
+        if value["categoryId"] not in results:
+            results[value["categoryId"]] = 0
+        results[value["categoryId"]] += 1
+
+    return results
+        
+
+def gatherChannelVideoData(youtube, channelId):
+    playlists = youtube.getChannelPlaylists(channelId)
+    itemsInPlaylist = list(set([videoId for playlistId in playlists for videoId in youtube.getItemsInPlaylist(playlistId)]))
+    stats = youtube.getVideoStatistics(itemsInPlaylist)
+
+    likes = sum([int(valueIfInDistOrDefault(i, "likeCount", 0)) for i in stats.values()])
+    dislikes = sum([int(valueIfInDistOrDefault(i, "dislikeCount", 0)) for i in stats.values()])
+    views = sum([int(valueIfInDistOrDefault(i, "viewCount", 0)) for i in stats.values()])
+    videoCount = len(itemsInPlaylist)
+    gategories = countGategories(stats)
+
+    return {"likes" : likes, "dislikes" : dislikes, "views" : views, "videoCount" : videoCount, "gategories" : gategories}
+
+def valueIfInDistOrDefault(d, k, default):
+    return d[k] if k in d else default
+
+def gatherChannelInfo(youtube, channelId):
+    info = youtube.getChannelInfo([channelId])
+    subCount = int(info[channelId]["statistics"]["subscriberCount"]) if not info[channelId]["statistics"]["hiddenSubscriberCount"] else None
+    title = info[channelId]["snippet"]["title"]
+    
+    return {"subCount" : subCount, "title": title}
+
+def gatherAllChannelInfo(youtube, channelNameOrId):
+    try:
+        channelId = youtube.getChannelId(channelNameOrId)
+        videoData = gatherChannelVideoData(youtube, channelId)
+        channelData = gatherChannelInfo(youtube, channelId)
+        
+        gategories = sorted(list(videoData["gategories"].items()), key=lambda i:i[1])
+        gategories = list(reversed([(youtube.getVideoGategory(i[0]), i[1]) for i in gategories]))
+        topGategories = [i[0] for i in gategories[:3]]
+        
+        out = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(channelData["title"], channelData["subCount"], videoData["likes"], videoData["dislikes"], videoData["views"], videoData["videoCount"], "\t".join(topGategories))
+        sys.stdout.write(out)
+    except Exception as ex:
+        print(ex)
+        print("Failed. video id {0}".format(channelNameOrId))
+        raise ex
+    
+def main():
+    youtube = Youtube.Youtube(apiKey.apiKey)
+    sys.stdout.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format("title", "subcriber count", "likes", "dislikes", "views", "video count", "top gategories"))
+    for i in sys.stdin:
+        sys.stderr.write("processing {0}\n".format(i))
+        channelName = i.replace("\n", "").replace("\r", "")
+        if len(i) == 0 or i.startswith("#"):
+            continue
+
+        gatherAllChannelInfo(youtube, channelName)
+        sys.stdout.flush()
+    
+
+if __name__ == "__main__":
+    main()
